68b63fc71d4798ac36d4c30d0007ffe8b9731795
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2023-01-25 10:03:28 +0100

    Remove Typings From .gitignore

---
+++ src/typings/index.ts
@@ -0,0 +1,17 @@
+
+export interface IPost {
+    id: number,
+    title: string,
+    content: string,
+    voteCount:number,
+    isFavorite: boolean,
+}
+
+export interface INavigationState {
+    currentComponent:
+      | "AllPostsComponent"
+      | "AddNewPostComponent"
+      | "FavoriteComponent"
+      | "EditPostComponent"
+      | "ViewPostComponent";
+  }
