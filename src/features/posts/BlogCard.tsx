9db2edb4e1d7d2af0cd30653d27e2f15e29b8962
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2023-01-25 12:19:25 +0100

    Fix Heart Icon Not Changing Color On Mobile Devices

--- src/features/posts/BlogCard.tsx
+++ src/features/posts/BlogCard.tsx
@@ -47,7 +47,7 @@ const BlogCard = (props: { posts: IPost[] }) => {
                 <span
                   className={post.isFavorite ? "is-favorite" : "not-favorite"}
                 >
-                  &#9829;
+                  {"❤"}
                 </span>
               </section>
               <section className="rating-section">
