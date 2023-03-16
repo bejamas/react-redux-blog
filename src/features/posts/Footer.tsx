35394a0e852f7a06fe55905f1d3d94835d85a1c6
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2023-03-16 10:57:49 +0100

    Implement Downvote and Upvote Post Feature

--- src/features/posts/Footer.tsx
+++ src/features/posts/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <footer>
         <p>
           Made with ❤ by 
-          <a href="https://github.com/tope-olajide">Temitope.js</a>
+           <a href="https://github.com/tope-olajide"> Temitope.js</a>
         </p>
       </footer>
     </>
